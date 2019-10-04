5bc595ff0bbc71db483b5fc4e9663d2adb701a1e
damian wajser <damianwajser@damiPc.local>
2019-10-04 20:22:56 -0300

    t

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+  .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
