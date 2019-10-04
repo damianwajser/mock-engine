dde612cacc0a2963278ab20c83c6c1b741be2964
damian wajser <damianwajser@damiPc.local>
2019-10-04 20:33:24 -0300

    t

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var mocks = require('./mocks');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+var app = express();
+
+app.use('/mocks', mocks);
+
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
