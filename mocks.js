e4cf02187d627f0de53245cb8ea30b27475282d0
damian wajser <damianwajser@damiPc.local>
2019-10-04 21:23:06 -0300

    post sessioon

--- mocks.js
+++ mocks.js
@@ -1,14 +1,57 @@
 const express = require('express')
 var router = express.Router();
 
+const testFolder = './mocks/';
+const fs = require('fs');
+
+var errors={};
+
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    fs.readFile(testFolder + file, (err, data) => {
+        try{
+            console.log("parseando....", data);
+            let json = JSON.parse(data);
+            console.log(json);
+            // define the home page route
+            createPath(router, json);
+        }catch(e){
+            console.error(e);
+            errors[file] = "error parser";
+        }
+    });
+  });
+});
+
+function createResponse(res, config){
+    res.status(config.status).json(config.response);
+}
+
+function createPath(router, config){
+    let path = '/'+config.path;
+    switch (config.method){
+        case "POST":
+            router.post(path, (req, res) => {
+                createResponse(res, config)
+            });
+            break;
+        default: 
+            router.get(path, (req, res) => {
+                createResponse(res, config)
+            });
+    }
+}
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
   });
+
+
+
   // define the home page route
-  router.get('/', function(req, res) {
-    res.send('Birds home page');
+  router.get('/errors', function(req, res) {
+    res.send(errors);
   });
-  
+
   module.exports = router;
