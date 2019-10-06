c954f6ed68e5f8bd4122402c65382d249d54b206
damian wajser <damianwajser@damiPc.local>
2019-10-06 15:25:43 -0300

    refactor

--- services/MockConfigurer.js
+++ services/MockConfigurer.js
@@ -6,20 +6,15 @@ const testFolder = './mocks/';
 const fs = require('fs');
 
 function getConfigFiles(){
-  return fs.readdirSync(testFolder).map(file => JSON.parse((fs.readFileSync(testFolder + file))));
+  return fs.readdirSync(testFolder)
+           .map(file => JSON.parse((fs.readFileSync(testFolder + file))));
 };
 
-function init (){
-  // middleware that is specific to this router
-  router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now());
-    next();
-  });
-  getConfigFiles().forEach(c=>mockService.createPath(router, c));
-}
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
 
-init();
-
-
+getConfigFiles().forEach(c => mockService.createPath(router, c));
 
 module.exports = router;
