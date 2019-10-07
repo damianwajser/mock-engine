5fe5a3f1f921bd791a97cc3810f5c8fc08603613
damian wajser <damianwajser@damiPc.local>
2019-10-07 13:36:39 -0300

    config database

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var mocks = require('./services/MockConfigurer');
+var test = require('./controllers/Test');
 
 var app = express();
 
 app.use('/mocks', mocks);
+app.use('/test', test);
 
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
